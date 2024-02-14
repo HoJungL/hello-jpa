bdea4695f6d525f2c5d9d11cc3c61095bf6130c4
HoJungL <ghaud5684@naver.com>
2024-02-14 14:31:58 +0900

    jpa_기본키 맵핑

---
+++ src/main/java/hellojpa/Member_key.java
@@ -0,0 +1,35 @@
+package hellojpa;
+
+import javax.persistence.*;
+
+@Entity
+@SequenceGenerator(name="member_seq_generator",
+sequenceName = "member_seq")
+public class Member_key {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String username;
+
+    public Member_key() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
